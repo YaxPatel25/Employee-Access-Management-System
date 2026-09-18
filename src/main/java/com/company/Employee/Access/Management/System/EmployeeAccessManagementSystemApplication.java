92d84549e906e0db54ee037e9c4ecdb476eb46d5
YaxPatel25 <np30201@gmail.com>
2026-09-18 17:33:37 +0530

    Initial Commit

---
+++ src/main/java/com/company/Employee/Access/Management/System/EmployeeAccessManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.company.Employee.Access.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAccessManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAccessManagementSystemApplication.class, args);
+	}
+
+}
